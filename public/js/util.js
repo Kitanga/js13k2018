44d994eef11d420ba1461f8abe1df664f800130a
Kitanga <kitanganday@gmail.com>
2018-08-19 13:41:50 +0200

    added JSDOcs to util.js

--- public/js/util.js
+++ public/js/util.js
@@ -1,13 +1,34 @@
+/**
+ * @type {Document}
+ */
 var doc = document;
 
-doc.on = function(type, callback) {
+/**
+ * Binds functions to events
+ * 
+ * @param {string} type The type of event
+ * @param {function(event)} callback The function that runs when the event fires
+ */
+doc.on = function (type, callback) {
     this.addEventListener(type, callback);
 };
 
-function util_getEle() {
+/**
+ * Get's an element from the DOM
+ * @param {string} selector CSS selector string
+ * @returns {HTMLElement}
+ */
+function util_getEle(selector) {
     return doc.querySelector(selector);
 }
 
+/**
+ * Creates an HTML element
+ * 
+ * @param {string} tag HTML tag
+ * @param {{property: number | string}} attributes An object of parameters to be passed on to the created element
+ * @returns {HTMLElement}
+ */
 function util_createElement(tag, attributes) {
     var element = doc.createElement(tag);
 
@@ -20,13 +41,19 @@ function util_createElement(tag, attributes) {
     return element;
 }
 
+/**
+ * 
+ * @param {number} width The width of the canvas
+ * @param {number} height The height of the canvas
+ * @param {function(CanvasRenderingContext2D)} callback The callback fired upon canvas creation
+ */
 function util_createCanvas(width, height, callback) {
     var _obj = {};
     _obj.width = width;
     _obj.height = height;
 
     var canvas = util_createElement('canvas', _obj);
-    callback(canvas.getContext('2d'));
+    callback ? callback(canvas.getContext('2d')) : '';
 
     return canvas;
 }
